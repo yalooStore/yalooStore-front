c9d47c6aee651100c91b8c1e3e1ad66c2e3f976b
hwiju yeom <yeom456@gmail.com>
2023-02-22 19:23:14 +0900

    .

--- front/src/main/java/com/yaloostore/front/common/controller/HomeController.java
+++ front/src/main/java/com/yaloostore/front/common/controller/HomeController.java
@@ -1,16 +1,32 @@
 package com.yaloostore.front.common.controller;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import static org.springframework.http.HttpHeaders.COOKIE;
 
 @Controller
 public class HomeController {
 
+    private final QuerydslOrderSystemService querydslOrderSystemService;
+
+
     @GetMapping("/")
-    public String home(){
+    public String main(Model model,
+                       @CookieValue(required = false, name = COOKIE)Cookie cookie,
+                       HttpServletResponse response){
+        model.addAttribute(
+                "bestseller",
+                querydslOrderSystemService.getBestSeller()
+        );
+
+
         return "main/index";
     }
 
