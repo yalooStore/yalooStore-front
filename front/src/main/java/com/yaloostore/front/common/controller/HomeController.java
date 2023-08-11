55a8ba3cbec110c3717d8d1fb2fea516690913bc
hwiju yeom <yeom456@gmail.com>
2023-08-11 22:16:01 +0900

    .

--- front/src/main/java/com/yaloostore/front/common/controller/HomeController.java
+++ front/src/main/java/com/yaloostore/front/common/controller/HomeController.java
@@ -1,7 +1,7 @@
 package com.yaloostore.front.common.controller;
 
 
-import com.yaloostore.front.auth.utils.CookieUtils;
+import com.yaloostore.front.common.utils.CookieUtils;
 import com.yaloostore.front.product.dto.response.ProductBookNewStockResponse;
 import com.yaloostore.front.product.service.inter.QuerydslProductSystemService;
 import jakarta.servlet.http.Cookie;
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 import static com.yaloostore.front.auth.utils.AuthUtil.HEADER_UUID;
-import static com.yaloostore.front.auth.utils.AuthUtil.JWT;
 import static org.springframework.http.HttpHeaders.COOKIE;
 
 @Controller
@@ -26,20 +25,19 @@ import static org.springframework.http.HttpHeaders.COOKIE;
 @Slf4j
 public class HomeController {
 
-    //private final QuerydslOrderSystemService querydslOrderSystemService;
 
     private final CookieUtils cookieUtils;
 
     private final QuerydslProductSystemService querydslProductSystemService;
 
+
+    //TODO: 해당 쿠키 네임이 COOKIE인게 맞는지 AUTH 관련 로직 살펴볼것.
     @GetMapping("/")
     public String main(Model model,
                        @CookieValue(required = false, name = COOKIE)Cookie cookie,
                        HttpServletResponse response, HttpServletRequest request){
 
         String uuid = cookieUtils.getUuidFromCookie(request.getCookies(), HEADER_UUID.getValue());
-
-
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
 
         log.info("name!!!!!!!! : {}", name);
--- front/src/main/java/com/yaloostore/front/product/controller/ProductWebController.java
+++ front/src/main/java/com/yaloostore/front/product/controller/ProductWebController.java
@@ -8,14 +8,13 @@ import com.yaloostore.front.product.dto.response.ProductDetailViewResponse;
 import com.yaloostore.front.product.dto.response.ProductTypeResponseDto;
 import com.yaloostore.front.product.service.inter.ProductTypeSystemService;
 import com.yaloostore.front.product.service.inter.QuerydslProductSystemService;
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -27,8 +26,9 @@ import java.util.Objects;
 @Slf4j
 public class ProductWebController {
 
-
     private final QuerydslProductSystemService querydslProductSystemService;
+
+
     /**
      * 사용자용 전체 상품 조회 view 반환 컨트롤러
      * */
